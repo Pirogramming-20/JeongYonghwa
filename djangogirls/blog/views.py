54628896a9b436e75763864c704d569d4c13f1ba
Eric <yonghwa814@naver.com>
2024-01-10 19:35:44 +0900

    Django 템플릿 + 오류수정

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now())\
         .order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts', posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 # blog.urls를 통해 매칭되는 view를 찾아 request를 넘김
 # view는 request를 받아서 처리하고 결과를 보냄
