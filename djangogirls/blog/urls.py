fa2ff024ba49519b1751751a99159ce6dd4e73a2
Eric <yonghwa814@naver.com>
2024-01-10 20:30:00 +0900

    애플리케이션 확장하기(url지정 심화+pk사용법(자동생성))

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+# 1. 기본주소/post에서
+# 2. 정수로 주어지는 변수 pk의 값을 가질 때
+# 3. 해당 요청을 post_detail view로 전송하고
+# 4. 그 view는 pk에 적절한 int을 부여하고, 그에 따라 내용을 처리한다
 
 # mysite.urls -> blog.urls로 전송요청 전파
 # 이때 경로는 둘다 ''->''이므로, 
--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -9,3 +9,9 @@ def post_list(request):
 
 # blog.urls를 통해 매칭되는 view를 찾아 request를 넘김
 # view는 request를 받아서 처리하고 결과를 보냄
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # 모델(Table) Post에서 기본키 = pk값에 해당하는 값을
+    # 탐색하고, 없으면 Page not found를 반환하라
+    return render(request, 'blog/post_detail.html', {'post':post})
