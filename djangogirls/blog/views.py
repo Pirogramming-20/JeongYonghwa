f9744374fbe7770c528e9a1aa2bcca9c7e5111bf
Eric <yonghwa814@naver.com>
2024-01-10 18:04:52 +0900

    Django View 만들기

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+# blog.urls를 통해 매칭되는 view를 찾아 request를 넘김
+# view는 request를 받아서 처리하고 결과를 보냄
